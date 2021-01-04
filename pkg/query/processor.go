3a5093d297f8c5e069fd721f6e5069de4f645229
Victor Ferreira <victor.ferreira@quintoandar.com.br>
2021-01-04 13:06:06 -0300

    job generation logic and small refactors

--- pkg/query/processor.go
+++ pkg/query/processor.go
@@ -1,8 +1,7 @@
 package query
 
 import (
-	"encoding/csv"
-	"os"
+	"fmt"
 	"time"
 )
 
@@ -10,7 +9,7 @@ func worker(id int, jobs <-chan []Query, results chan<- time.Duration) {
 	for j := range jobs {
 		fmt.Println("worker", id, "started  job", j)
 
-		for query := range j {
+		for _, query := range j {
 			elapsed := DoSelectQuery(query)
 			results <- elapsed
 		}
@@ -20,25 +19,40 @@ func worker(id int, jobs <-chan []Query, results chan<- time.Duration) {
 }
 
 func generateJobs(queries []Query) [][]Query {
-	//turn into map? 
+	var jobsMap = make(map[string][]Query)
+
+	for _, q := range queries {
+		jobsMap[q.Host] = append(jobsMap[q.Host], q)
+	}
+
+	var jobs [][]Query
+	for _, j := range jobsMap {
+		jobs = append(jobs, j)
+	}
+	return jobs
 }
 
 func PerformQueries(numWorkers int, queries []Query) []time.Duration {
 	var durations []time.Duration
 	numQueries := len(queries)
 
-	jobs := make(chan []Query)
+	jobsList := generateJobs(queries)
+	numJobs := len(jobsList)
+	jobs := make(chan []Query, numJobs)
 	results := make(chan time.Duration, numQueries)
 
 	for w := 1; w <= numWorkers; w++ {
 		go worker(w, jobs, results)
 	}
 
-	// generate jobs based on queries and feed them to job channel
-
+	for _, j := range jobsList {
+		jobs <- j
+	}
 	close(jobs)
 
 	for a := 1; a <= numQueries; a++ {
 		durations = append(durations, <-results)
 	}
+
+	return durations
 }
