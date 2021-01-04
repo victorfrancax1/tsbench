688733119a76f0bcaae101c42eb571e5f4b30d0a
Victor Ferreira <victor.ferreira@quintoandar.com.br>
2021-01-04 19:12:30 -0300

    adding prints for required metrics

--- cmd/select.go
+++ cmd/select.go
@@ -14,7 +14,7 @@ var selectCmd = &cobra.Command{
 	Use:   "select",
 	Short: "SELECT queries benchmark for TimescaleDB",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("number of workers:", workers)
+		fmt.Println("Number of workers:", workers)
 
 		queries, err := query.ProcessQueriesFile("query_params.csv")
 
@@ -22,12 +22,17 @@ var selectCmd = &cobra.Command{
 			fmt.Println("error:", err)
 		}
 
-		fmt.Println("No of supplied queries:", len(queries))
+		//fmt.Println("No of supplied queries:", len(queries))
 
 		elapsedList := query.PerformQueries(workers, queries)
 
 		fmt.Println("No of processed queries:", len(elapsedList))
-		fmt.Println(elapsedList)
+		//fmt.Println(elapsedList)
+		fmt.Println("Minimum query time:   ", elapsedList.Min())
+		fmt.Println("Maximum query time:   ", elapsedList.Max())
+		fmt.Println("Mean query time:      ", elapsedList.Mean())
+		fmt.Println("Average query time:   ", elapsedList.Average())
+		fmt.Println("Total processing time:", elapsedList.Sum())
 	},
 }
 
