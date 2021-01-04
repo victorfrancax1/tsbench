f9f5392d3b6e719654514a059a5a8c9f4370d40f
Victor Ferreira <victor.ferreira@quintoandar.com.br>
2021-01-04 19:11:56 -0300

    adding object for query times and metric extraction functions

--- pkg/query/processor.go
+++ pkg/query/processor.go
@@ -1,20 +1,66 @@
 package query
 
 import (
-	"fmt"
+	"sort"
 	"time"
 )
 
+type QueryTimes []time.Duration
+
+func (qt QueryTimes) Len() int { return len(qt) }
+
+func (qt QueryTimes) Swap(i, j int) { qt[i], qt[j] = qt[j], qt[i] }
+
+func (qt QueryTimes) Less(i, j int) bool { return qt[i] < qt[j] }
+
+func (qt QueryTimes) Average() time.Duration {
+	var sum time.Duration = 0
+	for _, t := range qt {
+		sum = sum + t
+	}
+	avg := float64(sum) / float64(len(qt))
+	return time.Duration(avg)
+}
+
+func (qt QueryTimes) Min() time.Duration {
+	sort.Sort(qt)
+	return qt[0]
+}
+
+func (qt QueryTimes) Max() time.Duration {
+	sort.Sort(qt)
+	return qt[len(qt)-1]
+}
+
+func (qt QueryTimes) Sum() time.Duration {
+	var sum time.Duration = 0
+	for _, t := range qt {
+		sum = sum + t
+	}
+	return sum
+}
+
+func (qt QueryTimes) Mean() time.Duration {
+	sort.Sort(qt)
+	mIndex := len(qt) / 2
+
+	if len(qt)%2 != 0 {
+		return qt[mIndex]
+	}
+
+	return (qt[mIndex-1] + qt[mIndex]) / 2
+}
+
 func worker(id int, jobs <-chan []Query, results chan<- time.Duration) {
 	for j := range jobs {
-		fmt.Println("worker", id, "started  job", j)
+		//fmt.Println("worker", id, "started  job")
 
 		for _, query := range j {
 			elapsed := DoSelectQuery(query)
 			results <- elapsed
 		}
 
-		fmt.Println("worker", id, "finished job", j)
+		//fmt.Println("worker", id, "finished job")
 	}
 }
 
@@ -32,8 +78,8 @@ func generateJobs(queries []Query) [][]Query {
 	return jobs
 }
 
-func PerformQueries(numWorkers int, queries []Query) []time.Duration {
-	var durations []time.Duration
+func PerformQueries(numWorkers int, queries []Query) QueryTimes {
+	var durations QueryTimes
 	numQueries := len(queries)
 
 	jobsList := generateJobs(queries)
